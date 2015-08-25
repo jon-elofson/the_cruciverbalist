60c3e7acdba74476587504d984d564ce8687d444
jon-elofson <jonathan.elofson@gmail.com>
2015-08-25 04:36:09 -0700

    migration update

--- db/migrate/20150813203746_drop_square_pos_array_col.rb
+++
@@ -1,5 +0,0 @@
-class DropSquarePosArrayCol < ActiveRecord::Migration
-  def change
-    remove_column :answers, :square_pos_array
-  end
-end
--- db/migrate/20150816001529_drop_answers.rb
+++ db/migrate/20150816001529_drop_answers.rb
@@ -1,6 +1,5 @@
 class DropAnswers < ActiveRecord::Migration
   def change
-    drop_table :answers
     remove_column :squares, :down_ans_id, :integer
     remove_column :squares, :across_ans_id, :integer
   end
