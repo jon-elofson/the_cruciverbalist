2b9866411aaba8ac9fe54e40b648695ccea8dc22
jon-elofson <jonathan.elofson@gmail.com>
2015-09-01 09:40:38 -0700

    add users api route

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,7 @@ Rails.application.routes.draw do
   resource :session
   namespace :api, defaults: { format: :json } do
     match 'puzzles/:id/fill_in_grid', to: 'puzzles#fill_in_grid', via: [:get]
+    resources :users
     resources :puzzles
     resources :squares
     resources :clues
